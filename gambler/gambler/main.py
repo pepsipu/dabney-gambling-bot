f5d0c42a3ca770cf5b3ca8be87dd756b70b66e9a
Sammy Hajhamid <sh@pepsi.pw>
2025-01-22 23:52:58 -0800

    simplyify code

--- gambler/gambler/main.py
+++ gambler/gambler/main.py
@@ -1,9 +1,8 @@
 import os
-import logging
 from dotenv import load_dotenv
-import asyncio
-import peewee as pw
-from telegram import Update, ChatFullInfo, Bot
+from itertools import starmap
+from sqlitedict import SqliteDict
+from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
@@ -14,45 +13,20 @@ from telegram.ext import (
 
 load_dotenv()
 
-db = pw.SqliteDatabase("/app/data/data.db")
-
-
-class User(pw.Model):
-    name = pw.CharField()
-
-    class Meta:
-        database = db
-
-
-class Roll(pw.Model):
-    msg = pw.IntegerField()
-    user = pw.ForeignKeyField(User, backref="rolls")
-
-    emoji = pw.CharField()
-    value = pw.IntegerField()
-
-    class Meta:
-        database = db
-
-
-db.connect()
-db.create_tables([User, Roll])
-
-
 BOT_TOKEN = str(os.getenv("TG_TOKEN"))
+DB_PATH = str(os.getenv("DB_PATH"))
+
+users = SqliteDict(DB_PATH)
+
 
 # logging.basicConfig(
 #     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 # )
 
 
-async def fetch_chat_details(bot: Bot, user_id, roll_count):
-    chat = await bot.get_chat(user_id)
-    return chat, roll_count
-
-
-def format_leaderboard_entry(index: int, chat: ChatFullInfo, roll_count: int) -> str:
-    return f"{index + 1}. {chat.username} - {roll_count}"
+def format_leaderboard_entry(index: int, id: str) -> str:
+    user = users[id]
+    return f"{index + 1}. {user["username"]} - {user["hits"]}"
 
 
 async def handle_leaderboard(
@@ -61,26 +35,16 @@ async def handle_leaderboard(
     if not (msg := update.message):
         return
 
-    # Perform a left join between User and Roll, counting rolls with value 64 for each user
-    query = (
-        User.select(User, pw.fn.COUNT(Roll.id).alias("roll_count"))  # type: ignore
-        .join(Roll, pw.JOIN.LEFT_OUTER)
-        .where(Roll.value == 64)
-        .group_by(User)
-        .order_by(pw.fn.COUNT(Roll.id).desc())  # type: ignore
-    )
-
-    bot = msg.get_bot()
-    tasks = [fetch_chat_details(bot, user.name, user.roll_count) for user in query]
-    results = await asyncio.gather(*tasks)
-
-    text = f"Leaderboard:\n" + "\n".join(
-        format_leaderboard_entry(i, result[0], result[1])
-        for i, result in enumerate(results)
-    )
-
     await msg.reply_text(
-        text,
+        f"Leaderboard:\n"
+        + "\n".join(
+            starmap(
+                format_leaderboard_entry,
+                enumerate(
+                    sorted(users, key=lambda id: users[id]["hits"], reverse=True)
+                ),
+            )
+        ),
         reply_to_message_id=msg.id,
     )
 
@@ -92,11 +56,17 @@ async def handle_roll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
         or not (author := msg.from_user)
     ):
         return
-    user, _ = User.get_or_create(name=author.id)
-    Roll.create(msg=msg.id, user=user, emoji=roll.emoji, value=roll.value)
+
+    # keep user up to date
+    user = users[author.id] if author.id in users else {"hits": 0, "username": None}
+    user["username"] = author.username
 
     if roll.emoji == "🎰" and roll.value == 64:
+        user["hits"] += 1
         await msg.reply_text("holy you're cracked", reply_to_message_id=msg.id)
+
+    users[author.id] = user
+    users.commit()
 
 
 def main() -> None:
